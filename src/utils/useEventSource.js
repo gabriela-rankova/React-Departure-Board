55faef5de495067dc3cb5bf5fd2b798118648207
Gabriela Rankova <grankova@a1.bg>
2021-12-05 16:36:32 +0200

    sort data, update data fix

--- src/utils/useEventSource.js
+++ src/utils/useEventSource.js
@@ -4,11 +4,17 @@ import { useEffect, useState, useRef } from "react";
 const getItems = (e) => {
   return typeof e.data !== "object" && JSON.parse(e.data);
 };
+const sortItems = (items) => {
+  return items.sort(
+    (a, b) =>
+      new Date(a.attributes.departure_time) -
+      new Date(b.attributes.departure_time)
+  );
+};
 
 const useEventSource = (url, options) => {
   const [data, updateData] = useState([]);
   const currentData = useRef([]);
-  const initial = useRef(true);
 
   useEffect(() => {
     currentData.current = data;
@@ -26,37 +32,25 @@ const useEventSource = (url, options) => {
     eventSource.onreset = (e) => {
       const items = getItems(e);
       console.log("reset");
-      if (initial.current) {
-        initial.current = false;
-
-        updateData(items);
-      } else {
-        items.map((item) => {
-          const itemIndex = currentData.current.find((obj) => {
-            return obj.id === item.id;
-          });
-          return (currentData.current[itemIndex] = item);
-        });
-
-        updateData(currentData.current);
-      }
+      updateData([...items]);
     };
 
     // when new resources are added, and it contains a single object
     eventSource.onadd = (e) => {
       console.log("add");
       const items = getItems(e);
-      updateData([...currentData.current, items]);
+      const newItems = sortItems([...currentData.current, items]);
+      updateData(newItems);
     };
     // when existing resources are updated, and it contains a single objwct
     eventSource.onupdate = (e) => {
       console.log("update");
       const item = getItems(e);
-      const itemIndex = currentData.current.find((obj) => {
+      const itemIndex = currentData.current.findIndex((obj) => {
         return obj.id === item.id;
       });
       currentData.current[itemIndex] = item;
-      updateData([...currentData.current]);
+      updateData([...sortItems(currentData.current)]);
     };
     // when a resource is removed, and it contains a single object
     eventSource.onremove = (e) => {
